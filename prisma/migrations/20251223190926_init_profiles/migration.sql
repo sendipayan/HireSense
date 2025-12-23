78fe1158338f04a573dce2b743cdb8eb073520e1
sendipayan <dipayansen461@gmail.com>
2025-12-24 00:47:18 +0530

    Candidate and Recuiter table added in Db

---
+++ prisma/migrations/20251223190926_init_profiles/migration.sql
@@ -0,0 +1,70 @@
+-- CreateEnum
+CREATE TYPE "CompanySize" AS ENUM ('SMALL', 'MEDIUM', 'LARGE', 'ENTERPRISE');
+
+-- CreateEnum
+CREATE TYPE "CandidateStatus" AS ENUM ('STUDENT', 'GRADUATE', 'WORKING_PROFESSIONAL');
+
+-- CreateEnum
+CREATE TYPE "ExperienceLevel" AS ENUM ('BEGINNER', 'INTERMEDIATE', 'ADVANCED');
+
+-- CreateEnum
+CREATE TYPE "JobType" AS ENUM ('INTERNSHIP', 'FULL_TIME', 'BOTH');
+
+-- CreateEnum
+CREATE TYPE "Availability" AS ENUM ('IMMEDIATE', 'ONE_TO_THREE_MONTHS', 'THREE_TO_SIX_MONTHS', 'LATER');
+
+-- CreateTable
+CREATE TABLE "Recruiter" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "jobTitle" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "companyName" TEXT NOT NULL,
+    "companyWebsite" TEXT NOT NULL,
+    "companyLinkedIn" TEXT,
+    "industry" TEXT NOT NULL,
+    "companySize" "CompanySize" NOT NULL,
+    "hiringForRoles" JSONB NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Recruiter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Candidate" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "status" "CandidateStatus" NOT NULL,
+    "institution" TEXT,
+    "graduationYear" INTEGER,
+    "degree" TEXT,
+    "primarySkills" JSONB NOT NULL,
+    "secondarySkills" JSONB,
+    "experienceLevel" "ExperienceLevel" NOT NULL,
+    "preferredRoles" JSONB NOT NULL,
+    "githubUrl" TEXT,
+    "linkedinUrl" TEXT,
+    "portfolioUrl" TEXT,
+    "jobTypePreference" "JobType",
+    "openToWork" BOOLEAN NOT NULL DEFAULT true,
+    "availability" "Availability",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Candidate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Recruiter_userId_key" ON "Recruiter"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Candidate_userId_key" ON "Candidate"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Recruiter" ADD CONSTRAINT "Recruiter_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Candidate" ADD CONSTRAINT "Candidate_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
