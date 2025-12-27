46ad35520a6d85a4d0182e82e54b5a47792bdc68
sendipayan <dipayansen461@gmail.com>
2025-12-28 00:07:42 +0530

    renamed middleware to proxy and verification added for post job

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyJwtEdge } from "@/lib/jwt_edge";
 import { cookies } from "next/headers";
 
-export default async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const cookieStore = await cookies();
   const token = cookieStore.get("auth_token")?.value;
@@ -12,17 +12,14 @@ export default async function middleware(req: NextRequest) {
     // Not logged in
 
     // Redirect logged-in users away from login
+    const payload = await verifyJwtEdge(token);
     if (pathname.startsWith("/login")) {
-      const payload = await verifyJwtEdge(token);
-
       if (payload) {
         return NextResponse.redirect(
           new URL(`/${payload.role.toLowerCase()}/dashboard`, req.url)
         );
       }
     }
-
-    const payload = await verifyJwtEdge(token);
 
     // Invalid / expired token
     if (!payload) {
@@ -38,6 +35,14 @@ export default async function middleware(req: NextRequest) {
 
     if (pathname.startsWith("/recruiter") && payload.role !== "RECRUITER") {
       return NextResponse.redirect(new URL("/", req.url));
+    }
+    if (
+      pathname.startsWith("/recruiter/post-job") &&
+      payload.isVerified !== "APPROVED"
+    ) {
+      return NextResponse.redirect(
+        new URL("/recruiter/dashboard/profile", req.url)
+      );
     }
 
     if (pathname.startsWith("/candidate") && payload.role !== "CANDIDATE") {
