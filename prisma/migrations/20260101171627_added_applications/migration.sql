fdcb71bfee3af183b016ecb1c5d9ff2322f42da1
sendipayan <dipayansen461@gmail.com>
2026-01-01 23:05:23 +0530

    added application model to prisma

---
+++ prisma/migrations/20260101171627_added_applications/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "ApplicationStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" TEXT NOT NULL,
+    "candidateId" TEXT NOT NULL,
+    "jobId" TEXT NOT NULL,
+    "status" "ApplicationStatus" NOT NULL DEFAULT 'PENDING',
+    "score" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_candidateId_fkey" FOREIGN KEY ("candidateId") REFERENCES "Candidate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "PostJob"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
